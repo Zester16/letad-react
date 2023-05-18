cfccc11f8a7f387da3560763094f3b8e7d945022
M Harsh <zesterschmid16@gmail.com>
2023-05-18 11:27:37 +0530

    feat/radioshackle: v 1.0.04 update loadingscreen for radioshackledash

--- src/views/radioshackleDash/Dash.js
+++ src/views/radioshackleDash/Dash.js
@@ -6,6 +6,7 @@ import imageLogo from "../../assets/images/image.svg"
 import streamLogo from "../../assets/images/stream.svg"
 import musicLogo from "../../assets/images/music.svg"
 import expandLogo from "../../assets/images/expand.svg"
+import SplashLoading from "../../generalRoutes/SplashLoading"
 
 /**
  * this is Dashboard for radio shackle which will display 
@@ -21,10 +22,12 @@ function RadioShackleDash() {
 	const [countRSStations, setCountRSStations] = useState()
 	const [countAllStations, setCountAllStations] = useState()
 	const [countAlert, setCountAlert] = useState()
+	const [splash, setSplash] = useState(false)
 	const { jwt } = useAuth()
 
 	useEffect(() => {
 		async function initFunction() {
+			setSplash(true)
 			await getStationCount()
 			await getAlertList()
 
@@ -46,43 +49,42 @@ function RadioShackleDash() {
 			console.log("station-count", alertList)
 			setCountAlert(alertList.message.length)
 			setAlertList(alertList.message)
-
+			setSplash(false)
 		}
 	}
 	return (<div>
-
-		<div className="g-div-flex">
-			<div className="g-div-card">
-				{countAllStations}
-				<div>
-					All Stations
+		{splash ? <SplashLoading></SplashLoading> :
+			<><div className="g-div-flex">
+				<div className="g-div-card">
+					{countAllStations}
+					<div>
+						All Stations
+					</div>
 				</div>
-			</div>
-			<div className="g-div-card">
-				{countAlert}
-				<div>
-					Stations with Issues
+				<div className="g-div-card">
+					{countAlert}
+					<div>
+						Stations with Issues
+					</div>
 				</div>
-			</div>
 
-		</div>
-		<div>
-			<div>
-				<table >
-					<tr>
-						<th>Station Name</th>
-						<th>Logo </th>
-						<th><img src={musicLogo} /></th>
-						<th><img src={imageLogo} /></th>
-						<th><img src={streamLogo} /></th>
-						<th><img src={expandLogo} /></th>
+			</div><div>
+					<div>
+						<table>
+							<tr>
+								<th>Station Name</th>
+								<th>Logo </th>
+								<th><img src={musicLogo} /></th>
+								<th><img src={imageLogo} /></th>
+								<th><img src={streamLogo} /></th>
+								<th><img src={expandLogo} /></th>
 
-					</tr>
-					{alertList.map(alert => <RadioShackleAlert issue={alert} />)}
-				</table>
+							</tr>
+							{alertList.map(alert => <RadioShackleAlert issue={alert} />)}
+						</table>
 
-			</div>
-		</div>
+					</div>
+				</div></>}
 	</div>)
 
 }
